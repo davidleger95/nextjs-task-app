e0097d476ba173b0389f82e2415c64e928025503
David Leger <davidleger95@gmail.com>
2023-12-10 21:43:43 -0400

    feat: update form to handle errors better

--- app/components/TaskForm/TaskForm.tsx
+++ app/components/TaskForm/TaskForm.tsx
@@ -13,27 +13,7 @@ import {
   TextField,
 } from '@radix-ui/themes';
 import { Task } from '../../../lib/redux';
-import { useState } from 'react';
-import DatePicker from 'react-datepicker';
-
-type InputError = {
-  field: keyof Omit<Task, 'id' | 'status'>;
-  message: string;
-};
-
-const validateTask = (task: Partial<Task>): InputError[] => {
-  const errors = [];
-  const hasTitle = (task.title?.length || 0) > 0;
-
-  if (!hasTitle) {
-    const titleError: InputError = {
-      field: 'title',
-      message: 'Title is required',
-    };
-    errors.push(titleError);
-  }
-  return errors;
-};
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 type Props = {
   title: string;
@@ -44,78 +24,78 @@ type Props = {
 };
 
 export default function TaskForm(props: Props) {
-  const [task, setTask] = useState<Partial<Task>>(
-    props.initialState || { title: '' }
-  );
-  const [errors, setErrors] = useState<InputError[]>([]);
-
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-
-    const formErrors = validateTask(task);
-    if (formErrors.length) {
-      setErrors(formErrors);
-      return;
-    }
-
-    props.submitAction(task);
+  const onSubmit: SubmitHandler<Partial<Task>> = (data) => {
+    props.submitAction(data);
   };
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setTask((prev) => ({ ...prev, [name]: value }));
-  };
+  const { handleSubmit, register, formState, getFieldState } = useForm<
+    Partial<Task>
+  >({
+    defaultValues: props.initialState || {},
+  });
+  console.log(formState.errors);
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Grid gap="4">
-        <Card style={{ overflow: 'visible' }}>
+        <Card>
           <Grid p="4" gap="4">
             <Heading>{props.title}</Heading>
+
             <label>
               <Grid gap="1">
                 <Text size="2">Title (required)</Text>
                 <TextField.Root>
                   <TextField.Input
-                    name="title"
+                    color={
+                      getFieldState('title').error?.message ? 'red' : undefined
+                    }
                     placeholder="What do you need to do?"
-                    required
-                    value={task.title}
-                    onChange={handleChange}
+                    {...register('title', {
+                      required: 'Title is required.',
+                      validate: (value) => !!value?.length,
+                    })}
+                    aria-invalid={formState.errors.title ? 'true' : 'false'}
                   />
                 </TextField.Root>
+                {formState.errors.title && (
+                  <Text size="1" color="red">
+                    {formState.errors.title.message}
+                  </Text>
+                )}
               </Grid>
             </label>
             <label>
               <Grid gap="1">
                 <Text size="2">Due Date</Text>
-                <DatePicker
-                  customInput={<TextField.Input />}
-                  name="dueDate"
-                  selected={task.dueDate ? new Date(task.dueDate) : undefined}
-                  onChange={(date) =>
-                    setTask((prev) => ({ ...prev, dueDate: date || undefined }))
-                  }
-                  minDate={new Date(Date.now())}
-                  placeholderText="Select a due date"
+                <TextField.Input
+                  placeholder="Select a due date"
+                  type="date"
+                  {...register('dueDate', {
+                    validate: (value) => {
+                      if (!value) return true;
+                      return new Date(value) > new Date(Date.now())
+                        ? true
+                        : 'Due Date must be in the future.';
+                    },
+                  })}
+                  aria-invalid={formState.errors.dueDate ? 'true' : 'false'}
                 />
+                {formState.errors.dueDate && (
+                  <Text size="1" color="red">
+                    {formState.errors.dueDate.message}
+                  </Text>
+                )}
               </Grid>
             </label>
             <label>
               <Grid gap="1">
                 <Text size="2">Description</Text>
-                <TextField.Root>
-                  <TextArea
-                    name="description"
-                    placeholder="More details about the task"
-                    value={task.description}
-                    onChange={handleChange}
-                  />
-                </TextField.Root>
+                <TextArea
+                  placeholder="More details about the task"
+                  {...register('description')}
+                />
               </Grid>
             </label>
-            {errors.length > 0 && <Text>Has Errors</Text>}
           </Grid>
         </Card>
         <Flex align="start" gap="2">
